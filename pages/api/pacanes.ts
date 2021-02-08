8d4de2926e294fb2b69fa5d2635e2e221430a02a
Stanislav Ivanchyk <stanislav.ivanchyk@student.tuke.sk>
2021-02-08 14:15:46 +0200

    pacanes test

--- pages/api/pacanes.ts
+++ pages/api/pacanes.ts
@@ -1,114 +1,108 @@
-// import fs from 'fs'
-// import path from 'path'
-// import * as tf from '@tensorflow/tfjs-node';
-// import { IncomingMessage } from 'http';
+import fs from 'fs'
+import path from 'path'
+import * as tf from '@tensorflow/tfjs-node';
+import { IncomingMessage } from 'http';
 
 
-export default async (req, res) => {
-    res.statusCode = 200;
-    res.json("ok")
+type queryParams = {
+    seed: string,
+    numGen: string
 }
 
+function isQuery(obj: any): obj is queryParams{
+    if(obj.seed === undefined) return false
+    if(obj.numGen === undefined) return false
+    return true;
+}
 
-// type queryParams = {
-//     seed: string,
-//     numGen: string
-// }
+export default async (req, res) => {
 
-// function isQuery(obj: any): obj is queryParams{
-//     if(obj.seed === undefined) return false
-//     if(obj.numGen === undefined) return false
-//     return true;
-// }
+    let errors = [];
+    //at first check if query has proper params
+    if(isQuery(req.query)){
 
-// export default async (req, res) => {
+        let {seed, numGen}: queryParams = req.query;
 
-//     let errors = [];
-//     //at first check if query has proper params
-//     if(isQuery(req.query)){
+        if(seed.length > 100) errors.push('seed length too big');
+        if(!isNaN(+numGen) && (+numGen > 500 || +numGen < 100)) errors.push('invalid numGen');
+    }else{
+        errors.push('invalid request params')
+    }
 
-//         let {seed, numGen}: queryParams = req.query;
+    //if no errors found handle a request
+    if(errors.length === 0){
+        res.statusCode = 200;
+        let modelPath =  'file://models/pacanes_model/model.json'
+        const model = await tf.loadLayersModel(modelPath);
+        let generated = await generateText(model, req.query.seed, +req.query.numGen);
+        res.json({ data: generated })
+    }else{
+        res.statusCode = 400;
+        res.json(errors);
+    }
 
-//         if(seed.length > 100) errors.push('seed length too big');
-//         if(!isNaN(+numGen) && (+numGen > 500 || +numGen < 100)) errors.push('invalid numGen');
-//     }else{
-//         errors.push('invalid request params')
-//     }
+}
 
-//     //if no errors found handle a request
-//     if(errors.length === 0){
-//         res.statusCode = 200;
-//         let modelPath =  'file://models/pacanes_model/model.json'
-//         const model = await tf.loadLayersModel(modelPath);
-//         let generated = await generateText(model, req.query.seed, +req.query.numGen);
-//         res.json({ data: generated })
-//     }else{
-//         res.statusCode = 400;
-//         res.json(errors);
-//     }
+const idx2char = ['\n', ' ', '!', '"', '#', '%', "'", '(', ')', '*', '+', ',', '-', '.', '/',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '?', '@', 'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z',
+    '[', ']', '_', '`', 'a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p',
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|', '\xa0', '©', '«', '¹', '»', 'ë', 'ʍ',
+    'ʏ', '̆', '̈', 'Β', 'Γ', 'Ε', 'Η', 'Κ', 'Μ', 'Π', 'α', 'δ', 'ρ', 'Ё', 'А', 'Б', 'В', 'Г',
+    'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
+    'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
+    'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь',
+    'э', 'ю', 'я', 'ё', 'і', 'ғ', 'ҕ', 'ӣ', 'ս', 'ᴀ', 'ᴄ', 'ᴇ', 'ᴋ', 'ᴍ', 'ᴏ', 'ᴘ', 'ᴛ',
+    '\u200d', '–', '—', '―', '…', '″', '™', '─', '☀', '☃', '☕', '☝', '☠', '☺', '♂', '♠',
+    '♥', '⚔', '⛓', '✊', '✌', '✍', '✏', '✖', '✝', '✟', '✨', '❄', '❌', '❓', '❤', '⠀',
+    '\u3000', '️', '🃏', '🇯', '🇵', '🌍', '🌑', '🌚', '🌝', '🌨', '🌪', '🌰', '🌲', '🌴', '🌸',
+    '🌹', '🍁', '🍂', '🍃', '🍑', '🍓', '🍹', '🍿', '🎁', '🎉', '🎊', '🎞', '🎧', '🎶', '🏆', '🏑',
+    '🏘', '🏻', '🏼', '🏽', '🏿', '🐊', '🐍', '🐪', '🐯', '🐼', '👀', '👄', '👆', '👇', '👉', '👊',
+    '👋', '👌', '👍', '👏', '👑', '👹', '💁', '💐', '💒', '💔', '💕', '💛', '💟', '💣', '💥', '💦',
+    '💨', '💪', '💫', '💯', '💰', '📌', '🔊', '🔞', '🔥', '🔫', '🕊', '🖕', '🖖', '🖤', '🗡', '😀',
+    '😁', '😂', '😃', '😄', '😅', '😆', '😇', '😈', '😉', '😊', '😋', '😌', '😍', '😎', '😏',
+    '😔', '😘', '😜', '😞', '😟', '😢', '😤', '😥', '😩', '😪', '😬', '😭', '😱', '😳', '😴', '😷',
+    '😹', '😺', '😻', '😼', '🙀', '🙂', '🙃', '🙈', '🙋', '🙌', '🙏', '🚀', '🚘', '🚥', '🚨', '🛎',
+    '🤔', '🤗', '🤘', '🤙', '🤜', '🤣', '🤤', '🤦', '🤩', '🤭', '🤷', '🥀', '🥃', '🥊', '🦁',
+    '🦅', '🦈', '🦋']
 
-// }
-
-// const idx2char = ['\n', ' ', '!', '"', '#', '%', "'", '(', ')', '*', '+', ',', '-', '.', '/',
-//     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '?', '@', 'A', 'B', 'C', 'D', 'E',
-//     'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z',
-//     '[', ']', '_', '`', 'a', 'b', 'c', 'd', 'e', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p',
-//     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '|', '\xa0', '©', '«', '¹', '»', 'ë', 'ʍ',
-//     'ʏ', '̆', '̈', 'Β', 'Γ', 'Ε', 'Η', 'Κ', 'Μ', 'Π', 'α', 'δ', 'ρ', 'Ё', 'А', 'Б', 'В', 'Г',
-//     'Д', 'Е', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
-//     'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к',
-//     'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь',
-//     'э', 'ю', 'я', 'ё', 'і', 'ғ', 'ҕ', 'ӣ', 'ս', 'ᴀ', 'ᴄ', 'ᴇ', 'ᴋ', 'ᴍ', 'ᴏ', 'ᴘ', 'ᴛ',
-//     '\u200d', '–', '—', '―', '…', '″', '™', '─', '☀', '☃', '☕', '☝', '☠', '☺', '♂', '♠',
-//     '♥', '⚔', '⛓', '✊', '✌', '✍', '✏', '✖', '✝', '✟', '✨', '❄', '❌', '❓', '❤', '⠀',
-//     '\u3000', '️', '🃏', '🇯', '🇵', '🌍', '🌑', '🌚', '🌝', '🌨', '🌪', '🌰', '🌲', '🌴', '🌸',
-//     '🌹', '🍁', '🍂', '🍃', '🍑', '🍓', '🍹', '🍿', '🎁', '🎉', '🎊', '🎞', '🎧', '🎶', '🏆', '🏑',
-//     '🏘', '🏻', '🏼', '🏽', '🏿', '🐊', '🐍', '🐪', '🐯', '🐼', '👀', '👄', '👆', '👇', '👉', '👊',
-//     '👋', '👌', '👍', '👏', '👑', '👹', '💁', '💐', '💒', '💔', '💕', '💛', '💟', '💣', '💥', '💦',
-//     '💨', '💪', '💫', '💯', '💰', '📌', '🔊', '🔞', '🔥', '🔫', '🕊', '🖕', '🖖', '🖤', '🗡', '😀',
-//     '😁', '😂', '😃', '😄', '😅', '😆', '😇', '😈', '😉', '😊', '😋', '😌', '😍', '😎', '😏',
-//     '😔', '😘', '😜', '😞', '😟', '😢', '😤', '😥', '😩', '😪', '😬', '😭', '😱', '😳', '😴', '😷',
-//     '😹', '😺', '😻', '😼', '🙀', '🙂', '🙃', '🙈', '🙋', '🙌', '🙏', '🚀', '🚘', '🚥', '🚨', '🛎',
-//     '🤔', '🤗', '🤘', '🤙', '🤜', '🤣', '🤤', '🤦', '🤩', '🤭', '🤷', '🥀', '🥃', '🥊', '🦁',
-//     '🦅', '🦈', '🦋']
-
-// const char2idx = {}
-// idx2char.forEach((e, index) =>{
-//     char2idx[e] = index;
-// })
+const char2idx = {}
+idx2char.forEach((e, index) =>{
+    char2idx[e] = index;
+})
 
 
-// //fucking python converted function
-// //I'm also wandering how it is fucking working
-// //new tool learned - ts-ignore
-// const generateText = async (model: tf.LayersModel, startString: string, numGen: number) =>{
+//fucking python converted function
+//I'm also wandering how it is fucking working
+//new tool learned - ts-ignore
+const generateText = async (model: tf.LayersModel, startString: string, numGen: number) =>{
 
-//     let numGenerate = numGen;
-//     let inputEval = [];
-//     startString.split('').forEach((val, index) => inputEval.push(char2idx[val]));
-//     let inputExtended = tf.expandDims(inputEval, 0);
+    let numGenerate = numGen;
+    let inputEval = [];
+    startString.split('').forEach((val, index) => inputEval.push(char2idx[val]));
+    let inputExtended = tf.expandDims(inputEval, 0);
 
-//     let textGenerated = [];
-//     const temperature = 0.2;
+    let textGenerated = [];
+    const temperature = 0.2;
 
-//     model.resetStates();
+    model.resetStates();
 
-//     for(let i = 0; i< numGenerate; i++){
-//         let predictions = model.predict(inputExtended);
-//         //@ts-ignore
-//         predictions = tf.squeeze(predictions, 0);
-//         //@ts-ignore
-//         predictions = tf.div(predictions, temperature);
+    for(let i = 0; i< numGenerate; i++){
+        let predictions = model.predict(inputExtended);
+        //@ts-ignore
+        predictions = tf.squeeze(predictions, 0);
+        //@ts-ignore
+        predictions = tf.div(predictions, temperature);
 
 
-//         //@ts-ignore
-//         let predictedId = tf.multinomial(predictions, 1);
+        //@ts-ignore
+        let predictedId = tf.multinomial(predictions, 1);
 
-//         let predictedArray = await predictedId.array();
-//         let idAsNum = predictedArray[predictedArray.length-1][0];
-//         inputExtended = tf.expandDims([idAsNum], 0);
-//         textGenerated.push(idx2char[idAsNum]);
+        let predictedArray = await predictedId.array();
+        let idAsNum = predictedArray[predictedArray.length-1][0];
+        inputExtended = tf.expandDims([idAsNum], 0);
+        textGenerated.push(idx2char[idAsNum]);
 
-//     }
-//     return startString+textGenerated.join('');
-// }
+    }
+    return startString+textGenerated.join('');
+}
